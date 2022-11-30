e6daa6ec056c6a0b9d88fd965933f7f7cacfa344
Miss. Bhagyashri Yadav <115793640+BhagyashriYadav@users.noreply.github.com>
2022-11-30 17:45:39 +0530

    Add files via upload

---
+++ image_php/upload.php
@@ -0,0 +1,42 @@
+<?php
+ 
+//connect to mysql server with username password and database name
+$connect = mysqli_connect("182.50.133.77", "prabudh", "Prabudh@123", "prabudhbharat");
+ 
+// Check connection
+if ($connect === false) {
+    die("Opps Unable to connect " . mysqli_connect_error());
+}
+ 
+// create query to select data
+$sql = "SELECT * FROM UploadImage";
+if ($result = mysqli_query($connect, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+        echo '<table border="1px">';
+        echo "<tr>";
+        echo "<th>id</th>";
+        echo "<th>image_path (INR)</th>";
+        echo "<th>CurName</th>";
+        //echo "<th>Image</th>";
+        echo "</tr>";
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            //echo "<td>" . $row['image_path'] . "</td>";
+            echo "<td>" . $row['CurName'] . "</td>";
+            echo "<td>" . "<img src=".$row['image_path'].' width=100px height="100px">' . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else {
+        echo "No records found";
+    }
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($connect);
+}
+ 
+// Close connection
+mysqli_close($connect);
+?>
